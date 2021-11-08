ac5d6541210a936fc005ecc8465e213c4452ed46
MadhanArts <madhanartsmadhan@gmail.com>
2021-11-08 13:07:34 +0530

    task3

--- Day1/task3.js
+++ Day1/task3.js
@@ -1,1 +1,15 @@
 // Write a JavaScript program to check the length of the string
+
+function lengthOfString(string) {
+    let length = 0;
+    let i = 0;
+    while (string[length] != undefined) {
+        length++;
+    }
+
+    return length;
+}
+
+console.log(lengthOfString("Reactjs"));
+console.log(lengthOfString("Bestenlist"));
+console.log(lengthOfString("30 days React.js Internship"));

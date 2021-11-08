bfd84970ee7e1f40644504f967a99fc68df723e2
MadhanArts <madhanartsmadhan@gmail.com>
2021-11-08 12:58:05 +0530

    task2

--- Day1/task2.js
+++ Day1/task2.js
@@ -1,1 +1,15 @@
-// Write a JavaScript program to check two given numbers and return true if one of the number is 50 or if their sum is 50
+// Write a JavaScript program to check two given numbers and return true if
+// one of the number is 50 or if their sum is 50
+
+function task2(a, b) {
+    if (a == 50 || b == 50 || a + b == 50) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(task2(20, 20));
+console.log(task2(50, 10));
+console.log(task2(20, 50));
+console.log(task2(40, 10));
